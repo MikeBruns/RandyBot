12eea90631bafc52ca8c3e181802a00cede5223d
Michael Bruns <michaelbruns99@gmail.com>
2017-05-04 01:24:36 -0400

    you can actually see the answers...

--- trivia_functions.js
+++ trivia_functions.js
@@ -8,30 +8,39 @@ var get_trivia_q = function(message) {
 		if("error" in json){
 			message.reply("Send Help! " + json.error.errors[0] + " - " + json.error.errors[0].reason);
 		}else{
-			message.reply(json.results[0].question);
-			console.log(`:: Asked a trivia question ::\n` + json.question + `\n`);
+			try{
+				message.reply(json.results[0].question);
+				console.log(`:: Asked trivia question ::\n---------------------------\n` + json.results[0].question + `\n`);
 
- 			if(json.results[0].type === "multiple"){
- 				var answers = [];
- 				answers.push(json.results[0].correct_answer);
- 				for(var i = 0; i < json.results[0].incorrect_answers.length; i++) {
- 					answers.push(json.results[0].incorrect_answers[i]);
- 				}
- 				randomize_array(answers);
- 			}
+				
+	 			if(json.results[0].type === "multiple"){
+	 				var answers = [];
+	 				answers.push(json.results[0].correct_answer);
+	 				for(var i = 0; i < json.results[0].incorrect_answers.length; i++) {
+	 					answers.push(json.results[0].incorrect_answers[i]);
+	 				}
+	 				randomize_array(answers);
 
- 			for(var i = 0; i < answers.length; i++){
- 				message.reply(answers[i]);
- 			}
+	 				message.reply(`\n 1) ` + answers[0] + 
+	 							  `\n 2) ` + answers[1] + 
+	 							  `\n 3) ` + answers[2] + 
+	 							  `\n 4) ` + answers[3]);
+	 			}else{
+	 				message.reply(`\n 1) True \n 2) False`)
+	 			}
 
-			console.log(`:: Answer to trivia question :: \n` + json.results[0].correct_answer);
+	 				
+				console.log(`:: Answer to trivia question ::\n-------------------------------\n` + json.results[0].correct_answer + `\n`);
+			}catch(err){
+				console.log(err.stack);
+			}
 		}
 	});
 }
 
 //Randomize the freaking array of answers so
 //puds can't guess the right answer each time
-var radomize_array = function(array){
+var randomize_array = function(array){
 	var currentIndex = array.length, temp, randomIndex;
 
 	while(0 !== currentIndex){
