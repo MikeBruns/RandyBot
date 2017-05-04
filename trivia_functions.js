da2419eb01769c204bd239c3a77ea479fab4f51a
Michael Bruns <michaelbruns99@gmail.com>
2017-05-04 00:49:30 -0400

    randomizing array of answers

--- trivia_functions.js
+++ trivia_functions.js
@@ -9,12 +9,41 @@ var get_trivia_q = function(message) {
 			message.reply("Send Help! " + json.error.errors[0] + " - " + json.error.errors[0].reason);
 		}else{
 			message.reply(json.results[0].question);
-				   // .then(msg => console.log(`:: Asked a trivia question ::\n` + json.question + `\n`))
- 				  //  .catch(console.error);
+			console.log(`:: Asked a trivia question ::\n` + json.question + `\n`);
 
-			console.log(`Answer to trivia question: ` + json.results[0].correct_answer);
+ 			if(json.results[0].type === "multiple"){
+ 				var answers = [];
+ 				answers.push(json.results[0].correct_answer);
+ 				for(var i = 0; i < json.results[0].incorrect_answers.length; i++) {
+ 					answers.push(json.results[0].incorrect_answers[i]);
+ 				}
+ 				randomize_array(answers);
+ 			}
+
+ 			for(var i = 0; i < answers.length; i++){
+ 				message.reply(answers[i]);
+ 			}
+
+			console.log(`:: Answer to trivia question :: \n` + json.results[0].correct_answer);
 		}
 	});
+}
+
+//Randomize the freaking array of answers so
+//puds can't guess the right answer each time
+var radomize_array = function(array){
+	var currentIndex = array.length, temp, randomIndex;
+
+	while(0 !== currentIndex){
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		temp = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temp;
+	}
+
+	return array;
 }
 
 module.exports.get_trivia_q = get_trivia_q;
